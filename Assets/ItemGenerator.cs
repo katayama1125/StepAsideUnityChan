c79e8f00dc4d03eca5978d14d9fd26ffc4d337f9
katayama1125 <76419146+katayama1125@users.noreply.github.com>
2021-01-26 21:09:22 +0900

    ゲーム内容修正
    障害物を順々に生成

--- Assets/ItemGenerator.cs
+++ Assets/ItemGenerator.cs
@@ -6,14 +6,22 @@ public class ItemGenerator : MonoBehaviour
     public GameObject carPrefab;
     public GameObject coinPrefab;
     public GameObject conePrefab;
+    public GameObject unitychan;
     private int startPos = 80;
     private int goalPos = 360;
     private float posRange = 3.4f;
+    private float positionZ = 80;
 
     // Use this for initialization
     void Start()
     {
-        for (int i = startPos; i < goalPos; i += 15)
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (unitychan.transform.position.z + 40 > positionZ && positionZ < goalPos)
         {
             int num = Random.Range(1, 11);
             if (num <= 2)
@@ -21,7 +29,7 @@ public class ItemGenerator : MonoBehaviour
                 for (float j = -1; j <= 1; j += 0.4f)
                 {
                     GameObject cone = Instantiate(conePrefab);
-                    cone.transform.position = new Vector3(4 * j, cone.transform.position.y, i);
+                    cone.transform.position = new Vector3(4 * j, cone.transform.position.y, positionZ);
                 }
             }
             else
@@ -33,26 +41,23 @@ public class ItemGenerator : MonoBehaviour
                     if (1 <= item && item <= 6)
                     {
                         GameObject coin = Instantiate(coinPrefab);
-                        coin.transform.position = new Vector3(posRange * j, coin.transform.position.y, i + offsetZ);
+                        coin.transform.position = new Vector3(posRange * j, coin.transform.position.y, positionZ + offsetZ);
                     }
                     else if (7 <= item && item <= 9)
                     {
                         GameObject car = Instantiate(carPrefab);
-                        car.transform.position = new Vector3(posRange * j, car.transform.position.y, i + offsetZ);
+                        car.transform.position = new Vector3(posRange * j, car.transform.position.y, positionZ + offsetZ);
                     }
                 }
             }
+                positionZ = positionZ + 15;
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
 
-void OnBecameInvisible()
+
+    void OnBecameInvisible()
     {
         GameObject.Destroy(this.gameObject);
     }
